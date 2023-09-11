557045b41f2211baadf4b8c6c039b32d1cd4da90
Kostas Minaidis <minaidisk@gmail.com>
2023-09-12 02:27:39 +0300

    React is now the default version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,12 @@
-module.exports = {
-  purge: [
-    './**/*.html',
-  ],
-  darkMode: false, // or 'media' or 'class'
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    extend: {},
     fontFamily: {
       'sans': ['Daikon', 'Helvetica', 'Arial', 'sans-serif']
-    },
-    letterSpacing: {
-      tightest: '-.075em',
-      tighter: '-.05em',
-      tight: '-.025em',
-      normal: '0',
-      wide: '.025em',
-      wider: '.05em',
-      widest: '.30em',
-    },
-    aspectRatio: {
-      none: 0,
-      square: [1, 1],
-      "16/9": [16, 9],
-      "4/3": [4, 3],
-      "21/9": [21, 9]
-    },
-    extend: {},
+    }
   },
-  variants: {
-    aspectRatio: ['responsive'],
-    extend: {},
-  },
-  plugins: [
-    require("tailwindcss-responsive-embed"),
-    require("tailwindcss-aspect-ratio"),
-  ],
+  plugins: [],
 }
+
